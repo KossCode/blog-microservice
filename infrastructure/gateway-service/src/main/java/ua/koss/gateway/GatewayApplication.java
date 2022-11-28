56b25f466a11f6ff8033aae46b64d126362cd320
k.kryvoshapka <itkonstantine@gmail.com>
2022-11-28 16:20:56 +0200

    removing unused libs declaration

--- infrastructure/gateway-service/src/main/java/ua/koss/gateway/GatewayApplication.java
+++ infrastructure/gateway-service/src/main/java/ua/koss/gateway/GatewayApplication.java
@@ -2,13 +2,7 @@ package ua.koss.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import ua.koss.gateway.configuration.AppConstant;
-
-import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableEurekaClient
